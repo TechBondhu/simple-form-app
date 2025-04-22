8aa7a0300f3a330175496f210b9d783422423de6
TechBondhu <meta500681@gmail.com>
2025-04-22 19:55:52 +0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,9 @@
 const firebaseConfig = {
- apiKey: "AIzaSyCoIdMx9Zd7kQt9MSZmowbphaQVRl8D16E",
- authDomain: "admissionformdb.firebaseapp.com",
- projectId: "admissionformdb",
- storageBucket: "admissionformdb.firebasestorage.app",   
- messagingSenderId: "398052082157",
- appId: "1:398052082157:web:0bc02d66cbdf55dd2567e4    
+  apiKey: "AIzaSyCoIdMx9Zd7kQt9MSZmowbphaQVRl8D16E",
+  authDomain: "admissionformdb.firebaseapp.com",
+  projectId: "admissionformdb.firebaseapp.com",
+  storageBucket: "admissionformdb.firebaseapp.com",
+  messagingSenderId: "admissionformdb.firebaseapp.com",
+  appId: "1:398052082157:web:0bc02d66cbdf55dd2567e4",
+  measurementId: "G-9B2QZBB5BE"
 };
