e59e39dd8a889e1c9d37d048c36a4f24062445ca
TechBondhu <meta500681@gmail.com>
2025-04-22 16:57:59 +0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,8 @@
-// firebase-config.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
-
 const firebaseConfig = {
-            apiKey: "AIzaSyCoIdMx9Zd7kQt9MSZmowbphaQVRl8D16E",
-            authDomain: "admissionformdb.firebaseapp.com",
-            projectId: "admissionformdb",
-            storageBucket: "admissionformdb.firebasestorage.app",
-            messagingSenderId: "398052082157",
-            appId: "1:398052082157:web:0bc02d66cbdf55dd2567e4   
+  apiKey: "your-api-key",
+  authDomain: "your-auth-domain",
+  projectId: "your-project-id",
+  storageBucket: "your-storage-bucket",
+  messagingSenderId: "your-messaging-sender-id",
+  appId: "your-app-id"
 };
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
-export { auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut };
